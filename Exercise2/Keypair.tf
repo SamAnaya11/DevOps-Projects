7b5f094025fd5f5b3d2b622746b18c4dd6d6414c
Sam <sam.anayagar11@gmail.com>
2025-06-05 21:56:03 -0600

    Update Keypair.tf

--- Exercise2/Keypair.tf
+++ Exercise2/Keypair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "dovekey" {
   key_name   = "dove-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN6GNJ9dLYT7m5dkBenfZQJRAhWARy2PTF36n3+zaEfv stray@DESKTOP-RPNGE55"
+  public_key = "ssh-ed25519 AAAAC3NzaC1dLYT7m5dkBenfZQJRAhWARy2PTF36n3+zaEfv stray@DESKTOP-RPNGE55"
 }
